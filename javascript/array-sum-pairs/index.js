42193576f53ce9ee09dd21efed78b99859e593a4
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-11 10:50:08 +0200

    Array-Sum-Pairs Challenge sollution

--- javascript/array-sum-pairs/index.js
+++ javascript/array-sum-pairs/index.js
@@ -1,3 +1,18 @@
 module.exports = function(array, sum) {
+    var map = {};
+    var current;
+    var diff;
 
+    for(var i=0, length=array.length; i<length; i++){
+        current = array[i];
+        diff = sum - current;
+
+        if (map.hasOwnProperty(diff)) {
+            return [diff, current];
+        }
+
+        map[current] = true;
+    }
+    
+    return undefined;
 };
