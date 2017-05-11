aec990116578d066009bd3e01070ca187a4539fe
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-11 11:32:44 +0200

    Math With Functions Solution

--- javascript/math-with-functions/index.js
+++ javascript/math-with-functions/index.js
@@ -1,16 +1,15 @@
-module.exports = {
-    one() {},
-    two() {},
-    three() {},
-    four() {},
-    five() {},
-    six() {},
-    seven() {},
-    eight() {},
-    nine() {},
+function factory(number){
+    return function() {
+        return arguments.length ? arguments[0].call(null, number) : number;
+    };
+}
 
-    plus() {},
-    minus() {},
-    times() {},
-    dividedBy() {}
-};
+module.exports = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].reduce(function(acc, item, index){
+    acc[item] = factory(index);
+    return acc;
+}, {
+    plus: function(a) {return function(b){return b+a}},
+    minus: function(a) {return function(b){return b-a}},
+    times: function(a) {return function(b){return b*a}},
+    dividedBy: function(a) {return function(b){return b/a}}
+});
